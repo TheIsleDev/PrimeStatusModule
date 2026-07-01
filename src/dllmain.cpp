c31735132037114937b51cbbaf826c2dec83f860
BlackCrystalic <39885003+BlackCrystalic@users.noreply.github.com>
2026-07-02 03:36:32 +0500

    ah better

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -11,8 +11,7 @@ private:
   const int tick_delay = 1200;
 
 public:
-  PrimeAnnouncer() : CppUserModBase()
-  {
+  PrimeAnnouncer() : CppUserModBase() {
     ModName = STR("PrimeAnnouncer");
     ModVersion = STR("1.0");
     ModDescription = STR("Tell players when they do something to prime tasks related");
@@ -31,15 +30,12 @@ public:
 };
 
 #define KISMET_DEBUGGER_MOD_API __declspec(dllexport)
-extern "C"
-{
-  KISMET_DEBUGGER_MOD_API RC::CppUserModBase* start_mod()
-  {
+extern "C" {
+  KISMET_DEBUGGER_MOD_API RC::CppUserModBase* start_mod() {
     return new PrimeAnnouncer();
   }
 
-  KISMET_DEBUGGER_MOD_API void uninstall_mod(RC::CppUserModBase* mod)
-  {
+  KISMET_DEBUGGER_MOD_API void uninstall_mod(RC::CppUserModBase* mod) {
     delete mod;
   }
 }
